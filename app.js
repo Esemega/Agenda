63df4db90e824135b5f1520167f10e354a1f0f4f
Esemega <sogaral89@gmail.com>
2020-11-16 19:02:45 +0100

    creating starting files and structure of the algorithm

---
+++ app.js
@@ -0,0 +1,39 @@
+//starting variables
+// Constants
+var WORK_HOURS = [
+    "08:00 - 09:00",
+    "09:00 - 10:00",
+    "10:00 - 11:00",
+    "11:00 - 12:00",
+    "12:00 - 13:00",
+    "13:00 - 14:00",
+    "15:00 - 16:00",
+    "16:00 - 17:00"
+  ];
+  
+  // Data
+  var myTeam = [
+    {
+      name: "María",
+      availability: new Array(8).fill(true)
+    },
+    {
+      name: "Pedro",
+      availability: new Array(8).fill(true)
+    },
+    {
+      name: "Esther",
+      availability: new Array(8).fill(true)
+    },
+    {
+      name: "Marcos",
+      availability: new Array(8).fill(true)
+    },
+  ];
+
+//Algorithm
+//1. Random availability generator
+//1.1. Generate the availability
+//1.2. Print the availability for each team member
+
+//2. Look for a the first free space for a meeting
